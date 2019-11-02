c74c7407e3638c9b9bf0f64410b5648462eaf9bf
Jay Bowers <jay.bowers@forgerock.com>
2019-11-02 18:01:58 +0000

    Make the engine a object

--- engine.js
+++ engine.js
@@ -1,6 +1,39 @@
-function run() {
+function Debug()  {
+	this.on = false;
+	this.onKeys = function(keys) {
+		if (keys.pressed('d')) {
+			this.on = ! this.on;
+		}
+	}
+	this.draw = function(ctx) {
+		if (this.on) {
+			ctx.fillText(
+			   	  'canvas.height = ' + canvas.height
+				+ ' canvas.width = ' + canvas.width,
+			12, 12);
+			callObjects(objects, o => 'debug' in o, o => o.debug(ctx));
+		}
+	} 
+}
+
+function Engine(objects) {
+	this.keys = new Keys();
+	objects.add(new Debug());
+	this.objects = objects;
+	_engine = this;
+	this.start = function() {
+		setInterval(function() {
+			_engine.run();
+		}, 100);
+		setInterval(function() {
+			_engine.draw();
+		}, 10);
+	}
+}
+
+Engine.prototype.run = function() {
 	for (let object of objects) {
-		callObject(object, o => 'onKeys' in o, o => o.onKeys(keys));
+		callObject(object, o => 'onKeys' in o, o => o.onKeys(this.keys));
 		if (object.coords) {
 			var oldCoords = object.coords.clone();
 			callObject(object, o => 'move' in o, o => o.move());
@@ -56,7 +89,7 @@ function circleCircleCollide(circle1, circle2) {
 	var distance = Math.sqrt( length * length + height * height );
 	return distance <= circle1.radius + circle2.radius;
 }
-function draw() {
+Engine.prototype.draw = function() {
 	var ctx = canvas.getContext("2d");
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	callObjects(objects, o => 'draw' in o, o => o.draw(ctx));
--- keys.js
+++ keys.js
@@ -1,16 +1,14 @@
 function Keys(listeners) {
 	this.listeners = listeners;
 	this.keys_pressed = new Set();
-	this.init = function() {
-		document.addEventListener('keydown', (event) => {
-//			console.log(event.type + ' ' + event.key);
-			this.keys_pressed.add(event.key);
-		});
-		document.addEventListener('keyup', (event) => {
-//			console.log(event.type + ' ' + event.key);
-			this.keys_pressed.delete(event.key);
-		});
-	};
+	document.addEventListener('keydown', (event) => {
+//		console.log(event.type + ' ' + event.key);
+		this.keys_pressed.add(event.key);
+	});
+	document.addEventListener('keyup', (event) => {
+//		console.log(event.type + ' ' + event.key);
+		this.keys_pressed.delete(event.key);
+	});
 	this.pressed = function(key) {
 		return this.keys_pressed.has(key);
 	}
