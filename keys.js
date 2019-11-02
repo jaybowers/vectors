2ceb015e37d62f0c0986b691057d70ec88f216aa
Jay Bowers <jay.bowers@forgerock.com>
2019-11-02 17:46:07 +0000

    Stop calling key down and keyup events

--- keys.js
+++ keys.js
@@ -5,12 +5,10 @@ function Keys(listeners) {
 		document.addEventListener('keydown', (event) => {
 //			console.log(event.type + ' ' + event.key);
 			this.keys_pressed.add(event.key);
-			callObjects(this.listeners, o => 'keydown' in o, o => o.keydown({ key: event.key }));
 		});
 		document.addEventListener('keyup', (event) => {
 //			console.log(event.type + ' ' + event.key);
 			this.keys_pressed.delete(event.key);
-			callObjects(this.listeners, o => 'keyup' in o, o => o.keyup({ key: event.key }));
 		});
 	};
 	this.pressed = function(key) {
